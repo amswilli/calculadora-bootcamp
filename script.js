5961b942b70db472a89a07f832cb7e5759a9fb77
Wilson Williams <57453694+amswilli@users.noreply.github.com>
2024-02-27 17:23:34 -0300

    Proyecto Calculadora de Hidratación terminado

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const CALCULAR = document.getElementById("calcular");
+const VOL = document.getElementById('vol')
 const FLU = document.getElementById("flu");
 const MAN = document.getElementById("man");
 const ERROR = document.getElementById("error");
 
 CALCULAR.addEventListener("click", () => {
-    const DATO = parseFloat(document.getElementById("peso").value);
+    const DATO = document.getElementById("peso").value;
     if(DATO <= 0){
         ERROR.style.display = 'block';
+        VOL.style.display = 'none'
         FLU.style.display = 'none';
         MAN.style.display = 'none';
     } else {
@@ -14,22 +16,24 @@ CALCULAR.addEventListener("click", () => {
         if (DATO <= 30) {
             let flujo = calcFlujo(DATO);
             let mantenimiento = flujo / 24;
-            FLU.innerHTML = parseInt(flujo) + ' cc/hr'; 
-            MAN.innerHTML = 'm+m/2 ' + parseInt(mantenimiento * 1.5) + ' cc/hr'; 
+            VOL.innerHTML = flujo + ' cc'
+            FLU.innerHTML = Math.round(mantenimiento) + ' cc/hr'; 
+            MAN.innerHTML = 'm+m/2 ' + Math.round(mantenimiento * 1.5) + ' cc/hr'; 
+            VOL.style.display = 'block'
             FLU.style.display = 'block';
             MAN.style.display = 'block';
         } else {
-            let  superCorp1 = (((DATO * 4) + 7) / (DATO + 90) * 1500);
-            let  superCorp2 = (((DATO * 4) + 7) / (DATO + 90) * 2000);
-            FLU.innerHTML = parseInt(superCorp1) + ' cc/hr o '+ parseInt(superCorp2) + ' cc/hr'; 
-            FLU.style.display = 'block';
+            let superCorporal = calcSuperficieCorporal(DATO);
+            VOL.innerHTML = Math.round(superCorporal * 1500) + ' cc o ' + Math.round(superCorporal * 2000) + ' cc'; 
+            VOL.style.display = 'block'
+            FLU.style.display = 'none';
             MAN.style.display = 'none';
         }
     }
 });
 
 function calcFlujo(peso){
-    let resto = peso; 
+    let resto = parseInt(peso); 
     let flujo = 0;
     if (resto > 20 && resto <= 30) {
         resto = (peso - 20) * 20;
@@ -41,4 +45,10 @@ function calcFlujo(peso){
         flujo = resto * 100;
     }
     return flujo;
+}
+
+function calcSuperficieCorporal(peso){
+    let resto = parseInt(peso);
+    let superCorp = ((resto * 4) + 7) / (resto + 90);
+    return superCorp;
 }
